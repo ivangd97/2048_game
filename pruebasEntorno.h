54d3309be2423855a996ccb389cdff18844955ea
Iván González <ivangd97@gmail.com>
2015-12-01 18:47:41 +0100

    Create pruebasEntorno.h

---
+++ pruebasEntorno.h
@@ -0,0 +1,19 @@
+#ifndef PRUEBASENTORNO_H_
+#define PRUEBASENTORNO_H_
+
+class pruebasEntorno {
+
+void pruebaEntornoIniciar();
+void pruebaPonerNumero();
+void pruebaEliminarCasilla();
+void pruebaMostrarMensaje();
+void pruebaMostrarMensajeFin();
+void pruebaPonerMarcadores();
+void pruebaPonerMaximo();
+void pruebaPonerPuntuacion();
+void pruebaEntornoPausa();
+void pruebaTipoTecla();
+
+};
+
+#endif /* PRUEBASENTORNO_H_ */
