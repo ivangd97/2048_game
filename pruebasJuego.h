44045a594ddbcb2e16bf5b584f209b096238851c
Iván González <ivangd97@gmail.com>
2016-09-14 19:48:14 +0200

    Create pruebasJuego.h

---
+++ pruebasJuego.h
@@ -0,0 +1,18 @@
+/*
+ * pruebasJuego.h
+ *
+ *  Created on: Jan 6, 2016
+ *      Author: ivangd97
+ */
+
+#ifndef PRUEBASJUEGO_H_
+#define PRUEBASJUEGO_H_
+#include "entorno.h"
+
+void pruebasPintarTablero();
+
+
+void pruebasJuego2048();
+
+
+#endif
