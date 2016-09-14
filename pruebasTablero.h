8b5d922d94b84ba4a4bf9c0e73e708adb80eff90
Iván González <ivangd97@gmail.com>
2016-09-14 19:50:00 +0200

    Create pruebasTablero.h

---
+++ pruebasTablero.h
@@ -0,0 +1,37 @@
+/*
+ * pruebasTablero.h
+ *
+ *  Created on: Jan 7, 2016
+ *      Author: ivangd97
+ */
+
+#ifndef PRUEBASTABLERO_H_
+#define PRUEBASTABLERO_H_
+
+void pruebasIniciar();
+
+void pruebasPuntuacionMax();
+
+void pruebasEstaLleno();
+
+void pruebasGenerar_inicial();
+
+void pruebasGenerar();
+
+void pruebasQueTamanio();
+
+void pruebasVacia();
+
+void pruebasObtenerValor();
+
+void pruebasDesplazarIzd();
+
+void pruebasDesplazarDcha();
+
+void pruebasDesplazarUp();
+
+void pruebasDesplazarDown();
+
+
+
+#endif /* PRUEBASTABLERO_H_ */
